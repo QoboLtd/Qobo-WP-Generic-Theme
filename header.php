3cedfb624b8d693f597d54086bcb700a3c4d5adb
Antonis Flangofas <a.flangofas@qobo.biz>
2015-08-21 12:38:34 +0300

    latest rendering mode for IE

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
 	<title><?php wp_title('', true, 'right'); ?></title>
 
